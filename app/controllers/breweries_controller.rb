40a47f6f5aadc571640ade9e2ba029470adb4676
Matthew Kimball <mekimba6@gmail.com>
2021-10-12 16:43:58 -0600

    adds more brewery pathing

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,7 +4,18 @@ class BreweriesController < ApplicationController
   end
 
   def new
+  end
 
+  def create
+    brewery = Brewery.new({
+      created_at: Time.now,
+      updated_at: Time.now,
+      brewery_name: params[:brewery][:brewery_name],
+      number_of_employees: params[:brewery][:number_of_employees],
+      has_food: params[:brewery][:has_food]
+    })
+    brewery.save
+    redirect_to "/breweries"
   end
 
   def show
@@ -14,10 +25,21 @@ class BreweriesController < ApplicationController
   def edit
     @breweries = Brewery.find(params[:id])
   end
-  
+
   def update
+    brewery = Brewery.find(params[:id])
+    brewery.update({
+      updated_at: Time.now,
+      brewery_name: params[:brewery][:brewery_name],
+      number_of_employees: params[:brewery][:number_of_employees],
+      has_food: params[:brewery][:has_food]
+    })
+    brewery.save
+    redirect_to "/breweries/#{brewery.id}"
   end
 
-  def delete
+  def destroy
+    Brewery.destroy(params[:id])
+    redirect_to "/breweries"
   end
 end
