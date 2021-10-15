fde7dd14f60e79cf2e34e9b570cad63d41df50d7
Alex McConnell <alexmmcconnell@gmail.com>
2021-10-14 20:08:57 -0600

    Feat - Add routing for breweries and beers web pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get '/breweries', to: 'breweries#index'
+  get '/breweries/:id', to: 'breweries#show'
+  get '/beers', to: 'beers#index'
+  get '/beers/:id', to: 'beers#show'
 end
