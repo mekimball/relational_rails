75a95b911d84c5848526b671d14fd0e84178be24
Alex McConnell <alexmmcconnell@gmail.com>
2021-10-19 12:59:01 -0600

    Refactor - Adjusted view and route variables for foods

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/breweries/:id/beers', to: 'beers#create'
   get '/breweries/:id/beers/new', to: 'beers#new'
   delete '/breweries/:id', to: 'breweries#destroy'
-  
+
   get '/breweries/:id/beers', to: 'beers#show_by_brewery'
 
   get '/beers', to: 'beers#index'
@@ -16,4 +16,23 @@ Rails.application.routes.draw do
   get '/beers/:id/edit', to: 'beers#edit'
   patch '/beers/:id', to: 'beers#update'
   delete '/beers/:id', to: 'beers#destroy'
+
+
+  get '/food_groups', to: 'food_groups#index'
+  get '/food_groups/new', to: 'food_groups#new'
+  post '/food_groups', to: 'food_groups#create'
+  get '/food_groups/:id/edit', to: 'food_groups#edit'
+  patch '/food_groups/:id', to: 'food_groups#update'
+  get '/food_groups/:id', to: 'food_groups#show'
+  post '/food_groups/:id/foods', to: 'foods#create'
+  get '/food_groups/:id/foods/new', to: 'foods#new'
+  delete '/food_groups/:id', to: 'food_groups#destroy'
+
+  get '/food_groups/:id/foods', to: 'foods#show_by_food_group'
+
+  get '/foods', to: 'foods#index'
+  get '/foods/:id', to: 'foods#show'
+  get '/foods/:id/edit', to: 'foods#edit'
+  patch '/foods/:id', to: 'foods#update'
+  delete '/foods/:id', to: 'foods#destroy'
 end
